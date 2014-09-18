4dd83ad3301a54969ecd13e556f62fda6bede86c
Chef <neil.appleton@arcangelegroupe.com>
2014-09-18 11:57:38 +0200

    Modif du controller pour afficher les concerts uniquement lorsqu'ils sont chargés

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,13 @@
 
 angular.module('appconfApp')
   .controller('MainCtrl', function ($scope, $http) {
+
     $scope.concerts = [];
+    $scope.concertsLoaded = false;
+
 	$http.get('http://nwx.bookdabun.fr/api/views/concerts')
 	  .then(function(result) {
+    	$scope.concertsLoaded = true;
 	    $scope.concerts = result.data;
 	});
   });
